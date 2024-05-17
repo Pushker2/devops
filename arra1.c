9cf3b58f84461c1fa82c1c1736164c71107a944f
Pushker2 <122859248+Pushker2@users.noreply.github.com>
2024-05-17 12:40:20 +0000

    Update arra1.c

--- arra1.c
+++ arra1.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 int main()
 {
-int arr[5];
-arr[0]=5;
-arr[2]=-10;
-arr[3/2]=2;
-arr[4/2];
-arr[3]=arr[0];
-
-printf("%d%d%d%d%d",arr[0],arr[1],arr[2],arr[3],arr[2]);
-
+printf("Hello i am running on docker");
 return 0;
 }
